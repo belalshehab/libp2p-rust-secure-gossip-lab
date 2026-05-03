9da0c806a8d4a7da6a8d597b72361dae399d1bb7
belalshehab <bbellall21@gmail.com>
2026-05-03 10:31:28 +0300

    gossip protocol on top of mdns

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ use libp2p::{
     Multiaddr, PeerId, SwarmBuilder,
     identity,
     mdns,
+    gossipsub,
     swarm::{NetworkBehaviour, SwarmEvent},
 };
 
+use tokio::io::{self, AsyncBufReadExt};
+
 #[derive(NetworkBehaviour)]
 struct MyBehaviour {
-    mdns: libp2p::mdns::tokio::Behaviour,
+    mdns: mdns::tokio::Behaviour,
+    gossipsub: gossipsub::Behaviour,
 }
 
 #[tokio::main]
@@ -28,72 +32,113 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Local peerId: {}", local_peer_id);
 
+    let mut gossipsub = gossipsub::Behaviour::new(
+        gossipsub::MessageAuthenticity::Signed(local_key.clone()),
+        gossipsub::Config::default(),
+    )?;
+
+    let topic = gossipsub::IdentTopic::new("Chat");
+    gossipsub.subscribe(&topic)?;
+
     let behaviour = MyBehaviour {
-        mdns: libp2p::mdns::tokio::Behaviour::new(mdns::Config::default(), local_peer_id)?,
+        mdns: mdns::tokio::Behaviour::new(mdns::Config::default(), local_peer_id)?,
+        gossipsub,
     };
 
     let mut swarm = SwarmBuilder::with_existing_identity(local_key)
-    .with_tokio()
-    .with_tcp(
-        Default::default(),
-        libp2p::noise::Config::new,
-        libp2p::yamux::Config::default,
-    )?
-    .with_behaviour(|_| behaviour)?
-    .build();
+        .with_tokio()
+        .with_tcp(
+            Default::default(),
+            libp2p::noise::Config::new,
+            libp2p::yamux::Config::default,
+        )?
+        .with_behaviour(|_| behaviour)?
+        .build();
 
-    let port = args.get(1)
+    let port = args
+        .get(1)
         .expect("Missing port")
         .parse::<u16>()
         .expect("Invalid port");
 
-    let dial = args.get(2)
-        .expect("missing dial")
-        .parse::<bool>()
-        .expect("missing dial");
-    
     let listen_addr: Multiaddr = format!("/ip4/0.0.0.0/tcp/{port}").parse()?;
     swarm.listen_on(listen_addr.clone())?;
     println!("Listening on port {}", port);
 
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
 
     loop {
-        match swarm.select_next_some().await {
-            SwarmEvent::NewListenAddr { address, ..} => {
-                println!("Now listening on {}", address);
-            }
-            SwarmEvent::ConnectionEstablished { peer_id, endpoint, ..} => {
-                println!("Connected to peer: {peer_id} via {endpoint:?}");
+        tokio::select! {
+            line = stdin.next_line() => {
+
+                let line = line?;
+                if let Some(line) = line {
+                    if let Err(err) = swarm.behaviour_mut().gossipsub.publish(topic.clone(), line.as_bytes()) {
+                        eprintln!("Publish error: {err}");
+                    }
+                }
             }
 
-            SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(event)) => match event {
-                mdns::Event::Discovered(peers) => {
-                    println!("Discoverd {} new peers", {peers.len()});
-                    for (peer, addr) in peers {
-                        if peer == local_peer_id {
-                            continue;
+            event = swarm.select_next_some() => {
+                match event {
+                    SwarmEvent::NewListenAddr { address, ..} => {
+                        println!("Now listening on {}", address);
+                    }
+                    SwarmEvent::ConnectionEstablished { peer_id, endpoint, ..} => {
+                        println!("Connected to peer: {peer_id} via {endpoint:?}");
+                        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+                    }
+
+                    SwarmEvent::ConnectionClosed { peer_id, cause, num_established, ..} => {
+                        println!("Disconnected from peer: {peer_id}, cause: {cause:?}");
+                        if num_established == 0 {
+                            swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
                         }
-                        println!("Discoverd peer: {:?}, addr: {:?}", peer, addr);
-                        if dial {
-                            println!("we will dial");
-                            if let Err(err) = swarm.dial(addr.clone()) {
-                                eprintln!("Failed to dial discovered peer {peer}: {err}");
+                    }
+
+                    SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(event)) => match event {
+                        mdns::Event::Discovered(peers) => {
+                            println!("Discoverd {} new peers", {peers.len()});
+                            for (peer, addr) in peers {
+                                if peer == local_peer_id {
+                                    continue;
+                                }
+                                println!("Discoverd peer: {:?}, addr: {:?}", peer, addr);
+                                let should_dial = local_peer_id.to_string() < peer.to_string();
+                                if should_dial {
+                                    println!("we will dial");
+                                    if let Err(err) = swarm.dial(addr.clone()) {
+                                        eprintln!("Failed to dial discovered peer {peer}: {err}");
+                                    } 
+                                } else {
+                                    println!("we will wait for them to dial us");
+                                }
                             }
-                        }
+                        },
+                        mdns::Event::Expired(peers) => {
+                            println!("Expired {} new peers", {peers.len()});
+                            for (peer, addr) in &peers {
+                                println!("Expired peer: {:?}, addr: {:?}", peer, addr);
+                            }
+                        },
                     }
-                },
-                mdns::Event::Expired(peers) => {
-                    println!("Expired {} new peers", {peers.len()});
-                    for (peer, addr) in &peers {
-                        println!("Expired peer: {:?}, addr: {:?}", peer, addr);
-                    }
-                },
-            }
 
-            event => {
-                println!("Swarm event: {event:?}");
+                    SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
+                        propagation_source,
+                        message_id,
+                        message,
+                    })) => {
+                        println!(
+                            "Got message from: {propagation_source}: '{}', with id: {message_id}",
+                            String::from_utf8_lossy(&message.data)
+                        );
+                    }
+
+                    event => {
+                        println!("Swarm event: {event:?}");
+                    }
+                }
             }
-            
         }
     }
 
